66c7dee8ee6b45fcf4a8a8c7d8c6869764b75926
Pallavi Arora <pallavi.aro17@gmail.com>
2023-03-27 21:21:51 +0530

    score updation synchronized

--- gui_client.py
+++ gui_client.py
@@ -1,14 +1,15 @@
 import socket
+import select
 import tkinter as tk
 global scores,marked,color,winner
 global scores
 scores={}
 
 global marked
-marked = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+marked = [0]*16
 
 global color
-color = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+color = [0]*16
 
 global winner
 winner=0
@@ -17,9 +18,12 @@ print("welcome")
 UDP_IP='127.0.0.1'
 UDP_PORT=6789
 
+global aux_msg
+aux_msg=False
+
 clientSock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-# clientSock.bind((UDP_IP,UDP_PORT))
+#clientSock.bind((UDP_IP,UDP_PORT))
 
 name=input("Enter your name:")
 clientSock.sendto(name.encode(), (UDP_IP, UDP_PORT))
@@ -30,31 +34,47 @@ opp_info = clientSock.recv(1024).decode().split(' ')
 opp_info[1] = opp_info[1][2:-1]
 print(opp_info)
 print("You are playing against ", opp_info[1])
+
+
+global ancdata
+ancdata = []
+
 global my_spy
 my_spy=input('Enter your choice of SPY grids(separater by ","): ').split(',')
 
 def init(name, opp_info):
-    global scores,marked,color,winner
-    scores[name]=0
-    scores[opp_info[1]]=0
+	global scores,marked,color,winner
+	scores[name]=0
+	scores[opp_info[1]]=0
 
 def update(name, num):
-    global scores,marked,color,winner
-    int_num=int(num)
-    button = buttons[int_num]
-    marked[int_num] = 1
-    if(num in my_spy):
-        color[int_num]='green'
-        button.config(bg="green")
-        scores[name] += 1
-        print(name+" scores a point ")
-    else:
-        color[int_num]='red'
-        button.config(bg="red")
+	global scores,marked,color,winner
+	int_num=int(num)
+	button = buttons[int_num]
+	marked[int_num] = 1
+	print(" I am in UPDATE with value ",num)
+	if(num in my_spy):
+		color[int_num]='blue'
+		button.config(bg="blue")
+		scores[name] += 1
+		scoremsg = "point scored for "+num
+		print(scoremsg)
+
+		print("I AM SENDING : ",scoremsg)
+		clientSock.sendmsg([scoremsg.encode()], ancdata, 0, (opp_info[2], int(opp_info[3])))
+
+	else:
+		color[int_num]='red'
+		button.config(bg="red")
+		losemsg = "no point scored for "+num
+		print(losemsg)
+		aux_msg=True
+		print("I AM SENDING : ",losemsg)
+		clientSock.sendmsg([losemsg.encode()], ancdata, 0, (opp_info[2], int(opp_info[3])))
     
 def no_winner(name, opp_name):
     global scores,marked,color,winner
-    if(scores[name]==2):
+    if(scores[name]==2 ):
         winner=1
         return False
     elif(scores[opp_name]==2):
@@ -66,28 +86,99 @@ def no_winner(name, opp_name):
 init(name, opp_info)
 
 def interact(send):
-    print("you clicked ",send)
-    if no_winner(name,opp_info[1]):
-        if 'client1' in opp_info[0]:
 
-            clientSock.sendto(send.encode(), (opp_info[2], int(opp_info[3])))
-            update(name, send)
-            receive = clientSock.recv(1024).decode()
-            if(int(send) < 8):
-                update(opp_info[1], receive)
-        else:
-            
-            receive = clientSock.recv(1024).decode()
-            update(opp_info[1], receive)
-            
-            clientSock.sendto(send.encode(), (opp_info[2], int(opp_info[3])))
-            if(int(send) >= 8):
-                update(name, send)
+   
+	print("you clicked ",send)
+
+    
+	if no_winner(name,opp_info[1]):
+
+		if 'client1' in opp_info[0]:
+
+			print("I AM SENDING : ",send)
+			clientSock.sendto(send.encode(), (opp_info[2], int(opp_info[3])))
+
+
+			rcvdata= clientSock.recvmsg(1024)
+			print("I AM RECEIVING from rcvdata: ",rcvdata)
+			#below
+			rcvdata=rcvdata[0].decode()
+			if( not rcvdata.isdigit()):
+				print("Recvd msg :",rcvdata)
+				if("no" not in rcvdata):
+					val=rcvdata[17:]
+					button = buttons[int(val)]
+					button.config(bg="green")
+				else :
+					val=rcvdata[20:]
+					button = buttons[int(val)]
+					button.config(bg="red")				
+				receive = clientSock.recv(1024).decode()
+				print("I AM RECEIVING  from receive: ",receive)
+				if(not receive.isdigit()):
+					print("Recvd msg :",receive)
+					if("no" not in receive):
+						val=receive[17:]
+						button = buttons[int(val)]
+						button.config(bg="green")
+					else :
+						val=receive[20:]
+						button = buttons[int(val)]
+						button.config(bg="red")
+
+				else:
+			        	update(opp_info[1], receive)
+			else:
+
+				update(opp_info[1], rcvdata)
+			
+		else:
+			rcvdata= clientSock.recvmsg(1024)
+			print("I AM RECEIVING from rcvdata: ",rcvdata)
+			#below
+			rcvdata=rcvdata[0].decode()
+			if( not rcvdata.isdigit()):
+				print("Recvd msg :",rcvdata)
+				if("no" not in rcvdata):
+					val=rcvdata[17:]
+					button = buttons[int(val)]
+					button.config(bg="green")
+				else :
+					val=rcvdata[20:]
+					button = buttons[int(val)]
+					button.config(bg="red")				
+				receive = clientSock.recv(1024).decode()
+				print("I AM RECEIVING  from receive: ",receive)
+				if(not receive.isdigit()):
+					print("Recvd msg :",receive)
+					if("no" not in receive):
+						val=receive[17:]
+						button = buttons[int(val)]
+						button.config(bg="green")
+					else :
+						val=receive[20:]
+						button = buttons[int(val)]
+						button.config(bg="red")
+
+
+
+				else:
+			        	update(opp_info[1], receive)
+			else:
+
+				update(opp_info[1], rcvdata)
+			print("I AM SENDING : ",send)
+			clientSock.sendto(send.encode(), (opp_info[2], int(opp_info[3])))
+			#update(name, send)
         
-    if not no_winner(name,opp_info[1]):
-       print("THE WINNER IS: "+opp_info[1])
-       quit()
-        # Set up the Tkinter GUI
+	if not no_winner(name,opp_info[1]):
+		winmsg="THE WINNER IS: " + opp_info[1]
+		print(winmsg)
+
+		print("I AM SENDING : ",send)
+		clientSock.sendmsg([winmsg.encode()], ancdata, 0, (opp_info[2], int(opp_info[3])))
+		quit()
+
 root = tk.Tk()
 
 root.title(name)
